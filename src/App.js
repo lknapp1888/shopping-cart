8d5ee0c4db2f2564271e824a9bb61601d75515ce
Lewis Knapp <lknapp1888@gmail.com>
2023-04-16 18:00:55 +0100

    add qty update functions

--- src/App.js
+++ src/App.js
@@ -7,32 +7,56 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  const addToCart = function ({title, price, id, images, description}) {
+  const addToCart = function ({ title, price, id, images, description }) {
     let newCart = cart;
-    newCart.push({title, description, price, images, id, qty: 1})
-    setCart(newCart)
-    console.log(newCart)
+    newCart.push({ title, description, price, images, id, qty: 1 });
+    setCart(newCart);
+    console.log(newCart);
     // add qty feature for duplicates
-  }
+  };
 
   const removeFromCart = function (id) {
     let newCart = cart;
-    newCart = newCart.filter(item => {return item.id !== id})
+    newCart = newCart.filter((item) => {
+      return item.id !== id;
+    });
+    setCart(newCart);
+  };
+
+  const updateQty = function (qty, id) {
+    let newCart = cart;
+    console.log(qty)
+    console.log(id)
+    newCart = newCart.map(item => {
+      if (item.id === id) {
+        item.qty = qty;
+        return item;
+      }
+      return item;
+    })
     setCart(newCart)
   }
-  
+
   return (
     <div className="App">
       <Nav></Nav>
       <div className="mainContainer">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop addToCart={addToCart}/>} />
-          <Route path="/Cart" element={<Cart cart={cart} removeFromCart={removeFromCart}/>} />
+          <Route path="/shop" element={<Shop addToCart={addToCart} />} />
+          <Route
+            path="/Cart"
+            element={
+              <Cart
+                cart={cart}
+                removeFromCart={removeFromCart}
+                updateQty={updateQty}
+              />
+            }
+          />
         </Routes>
       </div>
       <footer></footer>
--- src/Cart.js
+++ src/Cart.js
@@ -2,28 +2,32 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const Cart = function (props) {
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     let totalPrice = 0;
 
-    for (let i = 0; i < props.cart.length; i ++) {
-      totalPrice += (props.cart[i].qty * props.cart[i].price)
+    for (let i = 0; i < props.cart.length; i++) {
+      totalPrice += props.cart[i].qty * props.cart[i].price;
     }
 
-    setTotal(totalPrice)
-  })
+    setTotal(totalPrice);
+  });
   return (
     <div className="cartContainer">
-        <CartTable cart={props.cart} removeFromCart={props.removeFromCart}></CartTable>
+      <CartTable
+        cart={props.cart}
+        removeFromCart={props.removeFromCart}
+        updateQty={props.updateQty}
+      ></CartTable>
       <div className="cartSummary">
         <h2>Summary</h2>
         <div className="discountContainer">
-            <h3>Add discount Code</h3>
-            <div>
-                <input type="text"></input>
-                <button>Apply</button>
-            </div>
+          <h3>Add discount Code</h3>
+          <div>
+            <input type="text" maxLength={10}></input>
+            <button>Apply</button>
+          </div>
         </div>
         <p>Subtotal: {total}</p>
         <p>Order total: {total}</p>
@@ -36,30 +40,62 @@ const Cart = function (props) {
 export default Cart;
 
 const CartTable = function (props) {
-
-  const removeItem = function(e) {
-    props.removeFromCart(parseInt(e.target.id))
-  }
-    return (
-        <table className="cartTable">
-  <thead>
-    <tr>
-      <th>Title</th>
-      <th>Price</th>
-      <th>Quantity</th>
-      <th>Remove</th>
-    </tr>
-  </thead>
-  <tbody>
-{    props.cart.map((item) => (
-          <tr>
-          <td>{item.title}</td>
-          <td>{item.price * item.qty}</td>
-          <td>{item.qty}</td>
-          <td><button onClick={removeItem} id={item.id}>x</button></td>
+  const removeItem = function (e) {
+    props.removeFromCart(parseInt(e.target.id));
+  };
+  return (
+    <table className="cartTable">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Remove</th>
         </tr>
-    ))}
-  </tbody>
-</table>
-    )
-}
+      </thead>
+      <tbody>
+        {props.cart.map((item) => (
+          <CartRow
+            item={item}
+            removeFromCart={props.removeFromCart}
+            updateQtyArr={props.updateQty}
+          ></CartRow>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const CartRow = function ({ item, removeFromCart, updateQtyArr }) {
+  const [qty, setqty] = useState(item.qty);
+
+  const removeItem = function (e) {
+    removeFromCart(parseInt(e.target.id));
+  };
+
+  const updateQty = function (e) {
+    setqty(e.target.value);
+    updateQtyArr(e.target.value, item.id);
+  };
+
+  return (
+    <tr>
+      <td>{item.title}</td>
+      <td>{item.price * qty}</td>
+      <td>
+        <input
+          type="number"
+          value={qty}
+          onChange={updateQty}
+          min="1"
+          max="99"
+        ></input>
+      </td>
+      <td>
+        <button onClick={removeItem} id={item.id}>
+          x
+        </button>
+      </td>
+    </tr>
+  );
+};
